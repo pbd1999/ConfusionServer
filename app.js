ee43e9b4891df589f3a7c9a0a1efb74db54163ee
pbd1999 <deshmukhp1212@gmail.com>
2020-05-21 00:05:46 +0530

    rest api and mongodb part 1

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use('/users', usersRouter);
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
